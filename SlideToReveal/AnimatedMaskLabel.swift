6333b02284515676949fba5469093d4bb4c23c95
travisgillespie <tgilles34@hotmail.com>
2016-01-11 15:28:51 -0600

    added gradient layer to UIView AnimatedMaskLabel

--- SlideToReveal/AnimatedMaskLabel.swift
+++ SlideToReveal/AnimatedMaskLabel.swift
@@ -16,6 +16,24 @@ class AnimatedMaskLabel: UIView {
         let gradientLayer = CAGradientLayer()
         
         //Configure the gradient layer
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        
+        let colors = [
+            UIColor.blackColor().CGColor,
+            UIColor.whiteColor().CGColor,
+            UIColor.blackColor().CGColor
+        ]
+        
+        gradientLayer.colors = colors
+        
+        let locations = [
+            0.25,
+            0.5,
+            0.75
+        ]
+        
+        gradientLayer.locations = locations
         
         return gradientLayer
     }()
@@ -28,9 +46,11 @@ class AnimatedMaskLabel: UIView {
     
     override func layoutSubviews() {
         layer.borderColor = UIColor.greenColor().CGColor
+        gradientLayer.frame = bounds
     }
     
     override func didMoveToWindow() {
         super.didMoveToWindow()
+        layer.addSublayer(gradientLayer)
     }
 }
